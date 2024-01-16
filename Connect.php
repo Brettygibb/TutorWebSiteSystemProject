fe84acedfe3b107762adf96c92ea34956aed152a
Brett Gibbons <BGIBBONS01@mynbcc.ca>
2024-01-15 20:42:23 -0400

    Bretts work
    I did the login info for the student and only the student can login

---
+++ Connect.php
@@ -0,0 +1,12 @@
+<?php
+$database="nbcctutordb";
+$user = "root";
+$pass = "";
+$host = "localhost";
+
+$conn = mysqli_connect($host, $user, $pass, $database);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
---
+++ StudentLogin.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <form action = "StudentLoginProc.php" method = "post">
+        <input type = "text" name = "email" placeholder = "Email">
+        <input type = "text" name = "pass" placeholder = "Password">
+        <button type = "submit">Login</button>
+    </form>
+</body>
+</html>
