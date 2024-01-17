096bb11619edd4382d816841351bd1adb0376f5e
JaydenVeno <jaydenveno@hotmail.com>
2024-01-17 15:00:28 -0400

    made rough homepage

--- index.php
+++ index.php
@@ -6,12 +6,41 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>NBCC Tutoring</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="styles.css">
     </head>
     <body>
-        Hello World! Bretts test
-        <?php
-        // put your code here
-        ?>
+            <!--nav bar-->
+            <div id="container">
+                <nav>
+                <a href="#">Home</a>
+                <a href="#">Find a Tutor</a>
+                <a href="#">Sign In</a>
+                <a href="#">Create an Account</a>
+                </nav>
+            </div>
+
+        <!--wrapper fiv for main content-->
+        <div class="wrapper">
+        <main>
+            <!--banner photo-->
+            <img src="images/nbccLogo.png" class="nbccBanner" alt="" />
+
+            <h1>Welcome to the NBCC Tutoring Hub!</h1>
+
+            <p id="indexParagraph">Where academic excellence meets personalized support! 
+            Whether you're a student seeking a helping hand on your educational journey or a passionate individual eager to share your knowledge, 
+            our tutoring service is here to connect you. At NBCC, we believe in the power of collaboration and the transformative impact of one-on-one learning experiences. 
+            Students, sign up today to unlock the door to academic success with our dedicated and qualified tutors, ready to guide you through your courses and boost your confidence.
+            Tutors, join our community of educators, share your expertise, and make a positive impact on the academic lives of your peers. Together, 
+            let's embark on a journey of knowledge, growth, and achievement. Welcome to a place where learning knows no bounds – welcome to the NBCC Tutoring Hub!
+            </p>
+
+            <?php
+            // put your code here
+            ?>
+        </main>
+</div>
     </body>
 </html>
