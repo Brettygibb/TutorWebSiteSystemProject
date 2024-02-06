6882ac09224bb0466e5f975804d4a39827aec5fe
Brett Gibbons <BGIBBONS01@mynbcc.ca>
2024-02-06 09:18:08 -0400

    Update LoginProc.php

--- LoginProc.php
+++ LoginProc.php
@@ -3,6 +3,33 @@ include 'Connect.php';
 session_start();
 
 
+<<<<<<< Updated upstream
+=======
+    $sql = "SELECT UserID, FirstName, LastName, Email, PasswordHash, Role FROM users WHERE Email = ?";
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt, "s", $email);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_store_result($stmt);
+
+    if (mysqli_stmt_num_rows($stmt) == 1) {
+        mysqli_stmt_bind_result($stmt, $id, $fname, $lname, $email, $hashedPassword, $role);
+//password will not work unless the users password is hashed
+        if (mysqli_stmt_fetch($stmt) && password_verify($pass, $hashedPassword)) {
+            session_start();
+            $_SESSION['email'] = $email;
+            $_SESSION['id'] = $id;
+            $_SESSION['name'] = $fname;
+            $_SESSION['role'] = $role;
+            $_SESSION['loggedin'] = true;
+            header("Location: $redirect");
+            exit();
+        }
+    }
+
+    mysqli_stmt_close($stmt);
+}
+>>>>>>> Stashed changes
+
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $_POST['email'];
