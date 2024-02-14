2186da3f4c8613884da9acd247f15a7fdf122e1a
Brett Gibbons <BGIBBONS01@mynbcc.ca>
2024-02-14 16:31:47 -0400

    Update StudentHeader.php

--- Includes/StudentHeader.php
+++ Includes/StudentHeader.php
@@ -3,7 +3,7 @@
         <nav>
             <ul>
                 <li><a href="StudentDashBoard.php">Home</a></li>
-                <li><a href="#">Search Tutors</a></li>
+                <li><a href="TutorSearch.php">Search Tutors</a></li>
                 <li><a href="Procs/LogoutProc.php">Logout</a></li>
                 <li><a href="StudentEditProfile.php">Edit Profile</a></li>
             </ul>
