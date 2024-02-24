ea5331334087d69ad82e504bfc8628fedb9d57d6
aalfonsobarajas01 <aalfonsobarajas01@mynbcc.ca>
2024-02-24 11:28:33 -0400

    Update ViewAllSessions.php

--- ViewAllSessions.php
+++ ViewAllSessions.php
@@ -16,7 +16,7 @@ $row = mysqli_fetch_assoc($result);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Student Dashboard</title>
+    <title>All Sessions</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
