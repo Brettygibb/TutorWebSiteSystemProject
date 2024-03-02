d88e4fc9dd34b02fc0698aae3f8b670c8aa7cc1d
aalfonsobarajas01 <aalfonsobarajas01@mynbcc.ca>
2024-03-02 11:22:23 -0400

    Update TutorSubscribeCourseProc.php
    Included DB layer

--- TutorSubscribeCourseProc.php
+++ TutorSubscribeCourseProc.php
@@ -1,6 +1,14 @@
 <?php
 session_start();
-include 'Connect.php';
+//include 'Connect.php';
+
+include 'Database.php';
+
+//Create a new instance of DB class 
+$database= new Database($servername, $username, $password, $dbname);
+
+//Get the database connection 
+$conn= $database ->getConnection();
 
 $userid = $_SESSION['id'];
 
