123917059cb4a4b91a36280ca62b6ac8cde031bd
aalfonsobarajas01 <aalfonsobarajas01@mynbcc.ca>
2024-03-04 22:55:05 -0400

    Update TutorHeader.php
    Fixed tutor header including existing functionalities

--- Includes/TutorHeader.php
+++ Includes/TutorHeader.php
@@ -5,9 +5,10 @@
                 <li><a href="TutorDashBoard.php">Home</a></li>
                 <li><a href="TutorSubscribedCourses.php">Subscribed Courses</a></li>
                 <li><a href="TutorSubscribeCourse.php">Subscribe a New Course</a></li>
+                <li><a href="SwitchStudent.php">Switch as Student</a></li>
+                <li><a href="TutorEditProfile.php">Edit Profile</a></li>
                 <li><a href="#">Upcoming Sessions</a></li>
                 <li><a href="Procs/LogoutProc.php">Logout</a></li>
-                <li><a href="TutorEditProfile.php">Edit Profile</a></li>
                 <li><a href="TutorAvailability.php">Enter Availability</a>!
 </li>
             </ul>
