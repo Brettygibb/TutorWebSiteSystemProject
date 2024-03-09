341ca42176eeab6a5be498deaf4c2440b07e1f45
aalfonsobarajas01 <aalfonsobarajas01@mynbcc.ca>
2024-03-09 13:15:01 -0400

    Update StudentHeader.php
    Included Notifications

--- Includes/StudentHeader.php
+++ Includes/StudentHeader.php
@@ -3,6 +3,7 @@
         <nav>
             <ul>
                 <li><a href="StudentDashBoard.php">Home</a></li>
+                <li><a href="Notifications.php">Notifications</a></li>
                 <li><a href="TutorSearch.php">Search Tutors</a></li>
                 <li><a href="BecomeTutor.php">Become a Tutor</a></li>
                 <li><a href="SwitchTutor.php">Switch as Tutor</a></li>
