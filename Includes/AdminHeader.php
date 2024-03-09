bb1d720102af72bf0fc0bd18165e352abb064b01
aalfonsobarajas01 <aalfonsobarajas01@mynbcc.ca>
2024-03-09 13:19:25 -0400

    Update AdminHeader.php

--- Includes/AdminHeader.php
+++ Includes/AdminHeader.php
@@ -3,7 +3,7 @@
         <nav>
             <ul>
                 <li><a href="AdminDashboard.php">Home</a></li>
-                <li><a href="Notifications.php">Notifications</a></li>
+                <li><a href="NotificationsAdmin.php">Notifications</a></li>
                 <li><a href="ReviewBecomingTutorRequests.php">Review Becoming a Tutor</a></li>
                 <li><a href="ReviewRequests.php">Review New Courses for Tutors</a></li>
                 <li><a href="AddAdmin.php">Add another Admin</a></li>
