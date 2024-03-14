598bdccbefa41c73b175e4f9eda8c3d9978ae0c4
aalfonsobarajas01 <aalfonsobarajas01@mynbcc.ca>
2024-03-14 10:52:39 -0300

    Update tutor.php

--- tutor.php
+++ tutor.php
@@ -1,19 +1,35 @@
 <?php
 require_once 'User.php'; // Include the parent class definition
 
-class Tutor extends User {
-    private $tutorId;
+class Tutor extends User implements JsonSerializable {
+    protected $tutorId;
+    protected $classesTaught;
 
-    public function __construct($firstName, $lastName, $email, $tutorId) {
-        parent::__construct($firstName, $lastName, $email);
+    public function __construct($firstName=null, $lastName=null, $tutorId=null, $classesTaught = [], $email = null, $password= null) {
+        parent::__construct($firstName, $lastName, $email,$password);
         $this->tutorId = $tutorId;
+        $this->classesTaught = $classesTaught;
     }
 
+    
     public function getTutorId() {
         return $this->tutorId;
     }
 
     public function setTutorId($tutorId) {
         $this->tutorId = $tutorId;
+    }
+    
+
+    public function getClassesTaught() {
+        return $this->classesTaught;
+    }
+
+    public function setClassesTaught($classesTaught) {
+        $this->classesTaught = $classesTaught;
+    }
+
+    public function jsonSerialize(): mixed {
+        return get_object_vars($this);
     }
 }
