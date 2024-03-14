d095321bb65f91ab0b1473ccf0b174501021c27a
aalfonsobarajas01 <aalfonsobarajas01@mynbcc.ca>
2024-03-14 11:00:54 -0300

    updated

--- student.php
+++ student.php
@@ -5,7 +5,7 @@ class Student extends User {
     private $studentId;
     
 
-    public function __construct($firstName= null, $lastName= null, $email= null, $password= null, $studentId=null) {
+    public function __construct($firstName, $lastName, $email= null, $password= null, $studentId=null) {
         parent::__construct($firstName, $lastName, $email,$password);
         $this->studentId = $studentId;
     }
