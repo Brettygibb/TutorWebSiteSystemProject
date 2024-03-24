c3e6fd18cef1a928a1730a587f7e529c4207ae2d
Cpquiggyfarts <j.maherwatson@gmail.com>
2024-03-24 12:26:17 -0300

    small update

--- storedProcs.sql
+++ storedProcs.sql
@@ -13,3 +13,21 @@ WHERE TRIM(LOWER(users.FirstName)) LIKE TRIM(LOWER(CONCAT('%', searchSubmit, '%'
    OR TRIM(LOWER(courses.CourseName)) LIKE TRIM(LOWER(CONCAT('%', searchSubmit, '%')));
 END$$
 DELIMITER ;
+
+
+DELIMITER $$
+CREATE PROCEDURE GetUserByEmail (
+IN userEmail VARCHAR(255))
+BEGIN
+select * from users where email = userEmail;
+END$$
+DELIMITER ;
+
+DELIMITER$$
+CREATE PROCEDURE AddUser(
+    IN p_FirstName VARCHAR(255), IN p_LastName VARCHAR(255), IN p_Email VARCHAR(255), IN p_PasswordHash VARCHAR(255), IN p_ConfirmationToken VARCHAR(255))
+BEGIN
+INSERT INTO users (FirstName, LastName, Email, PasswordHash, ConfirmationToken)
+    VALUES (p_FirstName, p_LastName, p_Email, p_PasswordHash, p_ConfirmationToken);
+END$$
+DELIMITER ;
