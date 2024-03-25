df9f6278bbd7a79c72283e28a5c90bc25c5d8ea7
aalfonsobarajas01 <aalfonsobarajas01@mynbcc.ca>
2024-03-25 19:52:03 -0300

    fixed
    fixed

--- Includes/TutorHeader.php
+++ Includes/TutorHeader.php
@@ -3,7 +3,45 @@
         <nav>
             <ul class="horizontal-nav">
                 <li><a href="TutorDashBoard.php">Home</a></li>
-                <li><a href="NotificationsTutor.php">Notifications</a></li>
+               <?php
+                // Check if the user is logged in
+                if (isset($_SESSION['id'])) {
+                    $userId = $_SESSION['id'];
+                    // Fetch unread notifications for the current user
+                    $sql = "SELECT * FROM notifications WHERE user_id = ? AND is_read = 0";
+                    $stmt = $conn->prepare($sql);
+                    if (!$stmt) {
+                        echo "Error: " . $conn->error;
+                        exit();
+                    }
+                    $stmt->bind_param("i", $userId);
+                    $stmt->execute();
+                    $result = $stmt->get_result();
+                    $unreadNotificationsCount = $result->num_rows;
+                    $stmt->close();
+
+                    // If there are unread notifications, apply a CSS style to change the color of the "Notifications" link
+                    if ($unreadNotificationsCount > 0) {
+                        echo '<li><a href="NotificationsTutor.php" style="color: red;">Notifications (' . $unreadNotificationsCount . ')</a></li>';
+                    } else {
+                        echo '<li><a href="NotificationsTutor.php">Notifications</a></li>';
+                    }
+                } else {
+                    // If the user is not logged in, display the link without any special styling
+                    echo '<li><a href="NotificationsTutor.php">Notifications</a></li>';
+                }
+                ?>
+
+                 
+                
+                
+                
+                
+                
+                
+                
+                
+                
                 <li><a href="TutorSubscribedCourses.php">Subscribed Courses</a></li>
                 <li><a href="TutorSubscribeCourse.php">Subscribe a New Course</a></li>
                 <li><a href="SwitchStudent.php">Student View</a></li>
