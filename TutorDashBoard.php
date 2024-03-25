2023faab724f14467203ed75c126284d2278a743
aalfonsobarajas01 <aalfonsobarajas01@mynbcc.ca>
2024-03-25 17:05:12 -0300

    Update TutorDashBoard.php

--- TutorDashBoard.php
+++ TutorDashBoard.php
@@ -51,7 +51,7 @@ $tutorStmt->close();
         <p>First Name: <?php echo htmlspecialchars($userDetails['FirstName']); ?></p>
         <p>Last Name: <?php echo htmlspecialchars($userDetails['LastName']); ?></p>
         <p>Email: <?php echo htmlspecialchars($userDetails['Email']); ?></p>
-        <p>Tutor ID: <?php echo htmlspecialchars($_SESSION['tutorId']); ?></p> <!-- Displaying TutorId -->
+        <!-- <p>Tutor ID: <?php echo htmlspecialchars($_SESSION['tutorId']); ?></p>  Displaying TutorId -->
         <?php if (!empty($userDetails['image'])): ?>
             <img src="<?php echo htmlspecialchars($userDetails['image']); ?>" alt="Profile Picture">
         <?php endif; ?>
