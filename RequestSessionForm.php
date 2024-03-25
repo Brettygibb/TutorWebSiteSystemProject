26df03d52a24ac7bac4d7436508281941c28b8c3
aalfonsobarajas01 <aalfonsobarajas01@mynbcc.ca>
2024-03-25 20:43:10 -0300

    Update RequestSessionForm.php

--- RequestSessionForm.php
+++ RequestSessionForm.php
@@ -29,7 +29,22 @@ $courseId = isset($_GET['courseId']) ? $_GET['courseId'] : '';
         <input type="hidden" name="endTime" value="<?php echo htmlspecialchars($endTime); ?>">
         <input type="hidden" name="courseId" value="<?php echo htmlspecialchars($courseId); ?>"> 
         
-        <!-- Add any additional form fields here -->
+        
+        <label for="firstName">First Name</label>
+        <input type="text" name="firstName" id="firstName" required>
+        <br>
+        <label for="lastName">Last Name</label>
+        <input type="text" name="lastName" id="lastName" required>
+        <br>
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email" required>
+        <br>
+        <label for="phone">Phone</label>
+        <input type="text" name="phone" id="phone" required>
+        <br>
+        <label for="subject">Subject</label>
+        <input type="text" name="subject" id="subject" required>
+        <br>
         <label for="message">Message:</label>
         <textarea id="message" name="message"></textarea><br>
         
