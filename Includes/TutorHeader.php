b212443d1bba4860d5cc2c68ccaf7a5823690c53
aalfonsobarajas01 <aalfonsobarajas01@mynbcc.ca>
2024-03-25 17:12:09 -0300

    Update TutorHeader.php

--- Includes/TutorHeader.php
+++ Includes/TutorHeader.php
@@ -8,7 +8,8 @@
                 <li><a href="TutorSubscribeCourse.php">Subscribe a New Course</a></li>
                 <li><a href="SwitchStudent.php">Switch as Student</a></li>
                 <li><a href="TutorEditProfile.php">Edit Profile</a></li>
-                <li><a href="UpcomingTutorSessions.php">Upcoming Sessions</a></li>
+                <li><a href="UpcomingTutorSessions.php">Upcoming Sessions - To Confirm</a></li>
+                <li><a href="UpcomingTutorSessionsConfirmed.php">Upcoming Sessions - Confirmed</a></li>
                 <li><a href="TutorAvailability.php">Enter Availability</a></li>
                 <li><a href="Procs/LogoutProc.php">Logout</a></li>
             </ul>
